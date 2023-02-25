d046ee33b7b3ce87aa1b6f8309bf55d1cee84a64
BotJB <69150052+BotJB@users.noreply.github.com>
2023-02-25 15:41:12 -0500

    Updated enviroment variables

--- data.sql
+++ data.sql
@@ -5,7 +5,7 @@ CREATE TABLE todo(
     email VARCHAR(255),
     title VARCHAR(255),
     progress VARCHAR(30),
-    data VARCHAR(255)
+    date VARCHAR(255)
 );
 
 CREATE TABLE users (
--- db.js
+++ db.js
@@ -4,10 +4,10 @@ require('dotenv').config()
 
 
 const pool=new Pool({
-user:process.env.DB_USER,
-password:process.env.DB_PASS,
-host:process.env.DB_HOST,
-port:process.env.DB_PORT,
+user:process.env.USERNAME,
+password:process.env.PASSWORD,
+host:process.env.HOST,
+port:process.env.PORT,
 database:process.env.DATABASE
 })
 
