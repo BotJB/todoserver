1e8490a89d7e651c601737d8cad0b9509c8a3e83
BotJB <69150052+BotJB@users.noreply.github.com>
2023-02-26 12:49:49 -0500

    Changes

--- server.js
+++ server.js
@@ -1,53 +1,67 @@
 const PORT=process.env.PORT || 8000
-const pool =require('./db')
+const pool =require('./models/db')
 const express=require('express')
 const {v4:uuidv4}=require('uuid')
 const jwt=require('jsonwebtoken')
 const app=express();
+require('./models/db')
+const userSchema=require('./models/userSchema')
+const postSchema=require('./models/todoSchema')
 const bcrypt=require('bcrypt')
 const cors=require('cors')
+const { default: mongoose } = require('mongoose')
 app.use(cors())
 app.use(express.json())
-app.get("/todos/:email",async (req,res)=>{
-    console.log('this route was hit')
-    const {email}=req.params
-    console.log(email)
-    try{
-      const data=await pool.query("SELECT * FROM  todo where email=$1",[email])
-      res.json(data.rows)
-    }
-    catch(err) {
-        console.log(err)
-    }
+
+
+
+app.get('/',async(res,req)=>{
+  res.send('hello there')
 })
+//this method is to get all the todos for a specific user
+app.get("/todos/:email",async (req,res)=>{
+  
+    const {email}=req.params
+   
+
+    const todos=await postSchema.find()
+
+   
+
+    res.status(200).json({todos})
+   
+})
+
+//This method is to insert new posts into the table
 app.post('/todos',async(req,res)=>{
   const {email,title,progress,date}=req.body
-  const id=uuidv4()
-  console.log(id)
-  try{
-
-    const response=await pool.query("INSERT INTO todo(id,email,title,progress,date) VALUES($1,$2,$3,$4,$5)",[id,email,title,progress,date]);
-    res.send(response)
-  }
-  catch(err){
-    console.error(err)
-  }
+   const post=new postSchema({email,title,progress,date})
+   await post.save();
+ 
+   res.status(200).json({message:'It was inseted'})
 })
 
+
+//this method is to update a specific todo
 app.put('/todos/:id',async(req,res)=>{
   const {id}=req.params
   const {email,title,progress,date}=req.body
-  const query=pool.query('UPDATE todo SET email=$1,title=$2,progress=$3,date=$4 WHERE id=$5',[email,title,progress,date,id])
-  res.send(query)
-  console.log(id)
+  const postId=new mongoose.Types.ObjectId
+
+     const todo= await postSchema.findByIdAndUpdate(id,{email:email,title:title,progress:progress})
+
+
+     console.log(todo)
+     res.status(200).json({message:'Edited successfully'})
 })
+
 
 //A route to delete the todo
 app.delete('/todos/:id',async(req,res)=>{
    const {id}=req.params
-   console.log(id)
-   const response=pool.query('DELETE FROM todo where id=$1',[id])
-   res.send(response)
+   
+   await postSchema.findByIdAndDelete(id)
+   res.status(200).json({message:'deleted'})
 
 })
 
@@ -55,17 +69,17 @@ app.delete('/todos/:id',async(req,res)=>{
 app.post('/login',async(req,res)=>{
 
   const {email,password}=req.body
-  const user=await pool.query('SELECT * from users WHERE email=$1',[email])
-  console.log(user.rows)
 
-  if(!user.rows.length){
-    return res.json('User not Found')
-  }
-  const success=await bcrypt.compare(password,user.rows[0].password)
-  if(success){
+  const user=await userSchema.findOne({email:email})
+   if(!user){
+    res.status(404).json('user not found')
+   }
+   const success=bcrypt.compare(password,user.password)
+   if(success){
     const token=jwt.sign({email},'secret',{expiresIn:'1hr'})
-    res.json({email,token})
-  }
+    res.status(200).json({email,token})
+   }
+
 
 
 
@@ -76,10 +90,16 @@ app.post('/signup',async(req,res)=>{
   const {email,password}=req.body
   const salt=10
   const hash=bcrypt.hashSync(password,salt)
- const resp=pool.query('INSERT INTO users(email,password) VALUES($1,$2)',[email,hash])
- const token=jwt.sign({email},'secret',{expiresIn:'1hr'})
+  
+  const token=jwt.sign({email},'secret',{expiresIn:'1hr'})
 
- res.json({email,token})
+  const user=new userSchema({email:email,password:hash})
+
+  await user.save();
+ 
+  res.status(200).json({email,token})
+
+
   
 })
 
